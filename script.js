d5696d9cc12e47b5dcbe20b23dda570489553e9f
Emmanuel10303 <emmanuelogbonna10303@gmail.com>
2024-10-18 10:40:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Used  function expreesion for the addition
+const addition = function (num, num2) {
+  console.log(num + num2);
+  return num + num2;
+};
+addition(20, 50);
+
+// SUBTRACTION
+const subtraction = function (sub, sub2) {
+  console.log(sub - sub2);
+  return sub - sub2;
+};
+subtraction(50, 35);
+
+// DIVISION
+const division = function (Div, Div2) {
+  console.log(Div / Div2);
+  return Div / Div2;
+};
+division(100, 10);
+
+//MULTIPLICATION
+const multiplication = function (Multiply, Multiply2) {
+  console.log(Multiply * Multiply2);
+  return Multiply * Multiply2;
+};
+multiplication(5, 5);
